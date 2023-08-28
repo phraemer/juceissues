c9069603b0688bc637d36d2d4f40cb1512787432
James Swift <james@3dengineer.com>
2023-08-28 10:59:51 +0200

    Update to head of JUCE develop

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cmake -Wno-dev --warn-uninitialized -S . -B "./build/" -G "Xcode"
+cmake -Wno-dev --warn-uninitialized -S . -B "./build/" -G "Xcode" -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
